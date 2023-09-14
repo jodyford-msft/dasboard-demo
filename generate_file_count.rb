ee5b44fc977d3fe99b028bb3040ee5e237aa4561
jodyford-msft <102770118+jodyford-msft@users.noreply.github.com>
2023-09-14 11:42:08 -0400

    Create generate_file_count.rb

---
+++ generate_file_count.rb
@@ -0,0 +1,9 @@
+# generate_file_count.rb
+folder_path = "_questions" 
+file_count = Dir.glob(File.join(folder_path, "*")).count
+
+data = {
+  "file_count" => file_count
+}
+
+File.write("_data/file_count.yml", data.to_yaml)
