e6adb58975466fef963b43882be1ab59012e3188
jodyford-msft <102770118+jodyford-msft@users.noreply.github.com>
2023-09-14 14:45:34 -0400

    Update generate_file_count.rb

--- generate_file_count.rb
+++ generate_file_count.rb
@@ -1,6 +1,26 @@
 # generate_file_count.rb
 require 'yaml'
-folder_path = "_questions" 
-file_count = Dir.glob(File.join(folder_path, "*")).count
+folder_path = "_questions"
+phrase_to_search = "#Expert Answer"  
 
-File.write("_data/file_count.yml", file_count.to_yaml)
+# Initialize counters
+total_file_count = 0
+matching_file_count = 0
+
+# Iterate through the files in the directory
+Dir.glob(File.join(folder_path, "*")).each do |file_path|
+  # Read the contents of the file
+  file_contents = File.read(file_path)
+
+  # Check if the file contents contain the desired phrase
+  if file_contents.include?(phrase_to_search)
+    matching_file_count += 1
+  end
+
+  total_file_count += 1
+end
+
+# Write the counts to separate YAML data files
+File.write("_data/total_file_count.yml", total_file_count.to_yaml)
+File.write("_data/expert_answered_file_count.yml", matching_file_count.to_yaml)
+
