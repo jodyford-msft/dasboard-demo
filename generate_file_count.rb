4597e39e90c36a87caa29531d0a66bbc02518e2a
jodyford-msft <102770118+jodyford-msft@users.noreply.github.com>
2023-09-14 14:49:06 -0400

    Update generate_file_count.rb

--- generate_file_count.rb
+++ generate_file_count.rb
@@ -1,11 +1,13 @@
 # generate_file_count.rb
 require 'yaml'
 folder_path = "_questions"
-phrase_to_search = "#Expert Answer"  
+expert_phrase_to_search = "Expert Answer"  
+approved_phrase_to_search = "Approved"  
 
 # Initialize counters
 total_file_count = 0
-matching_file_count = 0
+expert_file_count = 0
+approve_file_count = 0
 
 # Iterate through the files in the directory
 Dir.glob(File.join(folder_path, "*")).each do |file_path|
@@ -13,8 +15,12 @@ Dir.glob(File.join(folder_path, "*")).each do |file_path|
   file_contents = File.read(file_path)
 
   # Check if the file contents contain the desired phrase
-  if file_contents.include?(phrase_to_search)
-    matching_file_count += 1
+  if file_contents.include?(expert_phrase_to_search)
+    expert_file_count += 1
+  end
+
+    if file_contents.include?(approved_phrase_to_search)
+      approved_file_count += 1
   end
 
   total_file_count += 1
@@ -22,5 +28,6 @@ end
 
 # Write the counts to separate YAML data files
 File.write("_data/total_file_count.yml", total_file_count.to_yaml)
-File.write("_data/expert_answered_file_count.yml", matching_file_count.to_yaml)
+File.write("_data/expert_answered_file_count.yml", expert_file_count.to_yaml)
+File.write("_data/approved_file_count.yml", approved_file_count.to_yaml)
 
