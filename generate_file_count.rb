0b92dbb2b6c23075c5c3d01f8ff0038e2051a8d0
jodyford-msft <102770118+jodyford-msft@users.noreply.github.com>
2023-09-14 15:01:42 -0400

    Update generate_file_count.rb

--- generate_file_count.rb
+++ generate_file_count.rb
@@ -1,26 +1,26 @@
 # generate_file_count.rb
 require 'yaml'
 folder_path = "_questions"
-expert_phrase_to_search = "### Expert Answer"  
-approved_phrase_to_search = "## Approved"  
+expert_phrase_to_search = "Expert Answer"  
+approved_phrase_to_search = "Approved"  
 
 # Initialize counters
 total_file_count = 0
 expert_file_count = 0
-approve_file_count = 0
+approved_file_count = 0  # Initialize to 0
 
 # Iterate through the files in the directory
 Dir.glob(File.join(folder_path, "*")).each do |file_path|
   # Read the contents of the file
   file_contents = File.read(file_path)
 
-  # Check if the file contents contain the desired phrase
+  # Check if the file contents contain the desired phrases
   if file_contents.include?(expert_phrase_to_search)
-      expert_file_count += 1
+    expert_file_count += 1
   end
 
   if file_contents.include?(approved_phrase_to_search)
-      approved_file_count += 1
+    approved_file_count += 1
   end
 
   total_file_count += 1
@@ -30,4 +30,3 @@ end
 File.write("_data/total_file_count.yml", total_file_count.to_yaml)
 File.write("_data/expert_answered_file_count.yml", expert_file_count.to_yaml)
 File.write("_data/approved_file_count.yml", approved_file_count.to_yaml)
-
