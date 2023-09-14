c558f3aa7cad6c3f911c04b412d017f84c56fb35
jodyford-msft <102770118+jodyford-msft@users.noreply.github.com>
2023-09-14 13:12:34 -0400

    Update generate_file_count.rb

--- generate_file_count.rb
+++ generate_file_count.rb
@@ -2,13 +2,5 @@
 require 'yaml'
 folder_path = "_questions" 
 file_count = Dir.glob(File.join(folder_path, "*")).count
-#file_count = Dir["folder_path/**/*"].length
 
-
-# data = {
-#   file_count
-# }
-
-#File.write("_data/file_count.yml", data.to_yaml)
-#File.write("_data/file_count.yml", file_count.to_s)
 File.write("_data/file_count.yml", file_count.to_yaml)
